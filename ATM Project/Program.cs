13ed54231a3448f507532cdf2384a29c2a31f3b2
Dhruv Rayat <dhruvrayat50@gmail.com>
2021-12-10 19:54:38 +1300

    Yes, I still got C#; Poggerz

--- ATM Project/Program.cs
+++ ATM Project/Program.cs
@@ -10,7 +10,38 @@ namespace ATM_Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("")
+            // Variables
+            string pin = "12345";
+            string cardNumber = "1234 5678 1111 1111";
+            string cardNumberGuess = "";
+            string pinGuess = "";
+
+            while (cardNumberGuess != cardNumber)
+            {
+                Console.Write("Enter your card Number: ");
+                cardNumberGuess = Console.ReadLine();
+            }
+
+            while (pinGuess != pin)
+            {
+                Console.Write("Enter your Pin Number: ");
+                pinGuess = Console.ReadLine();
+
+                if (pinGuess == pin)
+                {
+                    string x;
+                    Console.Write("How much money would you like to withdraw: ");
+                    x = Console.ReadLine();
+
+                    Console.WriteLine("You have withdrawed " + x + " dollars!");
+                    Console.WriteLine("Come back soon!");
+                    Console.ReadLine();
+                } else
+                {
+                    Console.WriteLine("Get lost idiot");
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
