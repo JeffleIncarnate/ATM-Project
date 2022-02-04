22c37ce92a63bec4da19cb987ea9e60078ae0907
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-02-05 09:06:53 +1300

    Server is Active?

--- ATM Project/ServerClass.cs
+++ ATM Project/ServerClass.cs
@@ -6,8 +6,24 @@ using System.Threading.Tasks;
 
 namespace ATM_Project
 {
-    internal class ServerClass
+    public class myRemoteClass: MarshalByRefObject
     {
+        public myRemoteClass()
+        {
+            // TO DO: Add constructor logic here.
+        }
 
+        public bool SetString(String sTemp)
+        {
+            try
+            {
+                Console.WriteLine("This string '{0}' has a length of {1}", sTemp, sTemp.Length);
+                return sTemp != "";
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
