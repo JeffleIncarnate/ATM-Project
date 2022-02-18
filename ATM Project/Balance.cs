637dc57b3c342d40b05d6c16e17c533104c85f35
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-02-18 20:16:55 +1300

    Added the balance Class, This will get a random number and send it back to the main document

--- ATM Project/Balance.cs
+++ ATM Project/Balance.cs
@@ -8,6 +8,14 @@ namespace ATM_Project
 {
     internal class Balance
     {
+        public int _balance { get; set; }
 
+        public int BalanceFull()
+        {
+            Random rnd = new Random();
+            int _balance = rnd.Next(1000, 10000);
+
+            return _balance;
+        }
     }
 }
